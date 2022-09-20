49205a58fc8c9fb6715580112b9871c1fbd9ac66
Jules Ntare <julesntare@gmail.com>
2022-09-20 22:05:12 +0200

    added project btn disabling feature

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -52,6 +52,7 @@ export default function AddProjectModal() {
             className='btn btn-primary'
             data-bs-toggle='modal'
             data-bs-target='#addProjectModal'
+            disabled={data.clients.length < 1 ? 'disabled' : ''}
           >
             <div className='d-flex align-items-center'>
               <FaList className='icon' />
