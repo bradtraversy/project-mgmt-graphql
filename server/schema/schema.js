a930fd18d6e4314cdc705b16e99308a82c73ce16
mattlumsden229 <121324815+mattlumsden229@users.noreply.github.com>
2023-03-06 08:13:53 -0600

    changed function for removing projects authored by deleted users to the new way per mongoose version 7.0.0 (.remove -> .deleteOne)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -102,7 +102,7 @@ const mutation = new GraphQLObjectType({
       resolve(parent, args) {
         Project.find({ clientId: args.id }).then((projects) => {
           projects.forEach((project) => {
-            project.remove();
+            project.deleteOne();
           });
         });
 
